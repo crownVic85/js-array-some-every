4bac89c6ca019118029a721c8cf3217b06dd074f
crownVic85 <victor0285@yahoo.com>
2023-04-23 20:15:06 -0700

    no dupes

--- some-every.js
+++ some-every.js
@@ -48,7 +48,11 @@ Examples:
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+    return arr.every(function(val){
+        return arr.indexOf(val) === arr.lastIndexOf(val);
+      });
+}
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
